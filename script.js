2ee3a19c71f055b77d81b6f5ebe5b352a4d6ab6e
EA - Isabelli Pedrosa Andrade <isabelli_pedrosa@educadventista.org>
2026-08-24 15:20:58 -0300

    socorro

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 async function buscarFilmes() {
     // acessar a rota GET do backend e exibir os filmes na tela
-    const resposta = await fetch("/movies");
+    const resposta = await fetch("https://isagabi-flix-32b.vercel.app/movies");
     const filmes = await resposta.json();
     const sectionFilmes = document.querySelector(".filmes");
 
